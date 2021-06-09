91629b1599dfdbc6516284f6d262766b922b1053
ludechen <1171387014@qq.com>
2021-06-09 22:32:03 +0800

    perf(appView): add icon use case

--- vuecli-getting-stared/src/main.js
+++ vuecli-getting-stared/src/main.js
@@ -2,7 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@fect-ui/themes/main.css'
 import Fect from './plugins/fect'
+/**
+ * yarn add @fect-ui/vue-icons -S
+ * or
+ * npm install @fect-ui/vue-icons -S
+ */
+import Icon from '@fect-ui/vue-icons'
+
 
 createApp(App)
   .use(Fect)
+  .use(Icon)
   .mount('#app')
